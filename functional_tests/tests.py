f01e5ad37d39225ce3f898637adef333142b7c1a
Rahul Varma <rahulv2504@gmail.com>
2015-01-27 20:32:08 +0400

    make functional_tests an app,use LiveServerTestCase

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,8 +1,8 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-import unittest
+from django.test import LiveServerTestCase
 
-class NewVisitorTest(unittest.TestCase):
+class NewVisitorTest(LiveServerTestCase):
 
 	def setUp(self):
 		self.browser = webdriver.Firefox()
@@ -20,7 +20,7 @@ class NewVisitorTest(unittest.TestCase):
 	def test_can_start_a_list_and_retrieve_it_later(self):
 	 	# Edith has heard about a .....
 	 	# checkout its homepage
-	 	self.browser.get('http://localhost:8000')
+	 	self.browser.get(self.live_server_url)
 
 	 	#she notices the page title and header mention to-do lists
 	 	self.assertIn('To-Do', self.browser.title)
@@ -41,6 +41,7 @@ class NewVisitorTest(unittest.TestCase):
 	 	# When she hits enter, the page updates and the page lists
 	 	# "1: Buy peacock feathers" as an item in the to-do list table
 	 	inputbox.send_keys(Keys.ENTER)
+	 	
 	 	self.check_for_row_in_list_table('1: Buy peacock feathers')
 
 	 	# There is still a text box inviting her to add another item. She 
@@ -51,12 +52,16 @@ class NewVisitorTest(unittest.TestCase):
 	 	inputbox.send_keys('Use peacock feathers to make a fly')
 	 	inputbox.send_keys(Keys.ENTER)
 
+	 	# The page updates again,and now shows both items on her list
 	 	self.check_for_row_in_list_table('1: Buy peacock feathers')
 	 	self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
 	 	
-		
-	 	self.fail('Finish the test!')
+	 	#Now a new user comes to the site
+	 	## Use a new browser session to make sure no info of 
+	 	## of Edith comes through from the cookies etc #
+	 	self.fail('Finish the test')
+
+	  
 
     # She visits that URL - her to-do list is still there.           
-if __name__ == '__main__':
-	unittest.main(warnings='ignore')
+
